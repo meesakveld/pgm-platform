f4806ced97812dcf6ed5d3053164aa931a07ed77
meesakveld <meesakveld@gmail.com>
2024-05-10 12:46:52 +0200

    Started on setup react project

---
+++ client/src/routes/routes.js
@@ -0,0 +1,38 @@
+export const ROUTES = {
+    home: {
+        path: '/',
+        title: 'Home',
+    },
+    opleiding: {
+        path: '/opleiding',
+        title: 'Opleiding',
+    },
+    portfolio: {
+        path: '/portfolio',
+        title: 'Portfolio',
+    },
+    portfolioDetail: {
+        path: '/portfolio/:slug',
+        title: 'Portfolio Detail',
+    },
+    blog: {
+        path: '/blog',
+        title: 'Blog',
+    },
+    blogDetail: {
+        path: '/blog/:slug',
+        title: 'Blog Detail',
+    },
+    services: {
+        path: '/services',
+        title: 'Services',
+    },
+    servicesDetail: {
+        path: '/services/:slug',
+        title: 'Services Detail',
+    },
+    team: {
+        path: '/team',
+        title: 'Team',
+    },
+}

4338135c0475b392d5967569b88d2dc17a8c00d5
meesakveld <meesakveld@gmail.com>
2024-05-28 14:22:20 +0200

    Refactored Opleiding page to use Apollo client and ListLayout component

--- client/src/pages/Opleiding.jsx
+++ client/src/pages/Opleiding.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react'
 
 import TopSection from '../components/TopSection'
-import GridLayout from '../components/GridLayout'
 
 import { useQuery } from '@apollo/client'
 import { GET_ALL_DATA_FOR_OPLEIDING_PAGE } from '../graphql/opleiding-queries'
+
+import deco3 from '../assets/decoration/deco-3.svg'
+import TitleSection from '../components/TitleSection'
 
 export default function Opleiding() {
 
@@ -18,28 +20,28 @@ export default function Opleiding() {
         if (!data) return
         setProgrammaLines(data.programmaLines)
         setPage(data.pages[0])
-        setImage(data.values)
+        setImage(data.image)
     }, [data])
-
-    const description = "In het Graduaat Programmeren leer je programmeren en staat front-end web development centraal. Jouw lessen worden doorspekt met interactieve oefeningen. Je krijgt opdrachten om nieuwe leerstof snel onder de knie te krijgen. Werkplekleren en jouw stage zijn samen goed voor meer dan 1.200 uur aan praktijkervaring. Hierna ben je meer dan klaar voor de arbeidsmarkt."
 
     return (
 
         <main className="max-w-200 mx-auto">
 
-            <TopSection title="Opleiding" description={description} />
-            
+            {page &&
+                <TopSection title={page.title} description={page.description} />
+            }
+
             {loading && <p className='p-6'>Loading...</p>}
             {error && <p className='p-6'>Error: {error.message}</p>}
 
-            {data && data.programmaLines && 
+            {programmaLines &&
                 <div className="p-6">
 
                     <h2 className="font-manuka text-6xl sm:text-8xl uppercase mb-8 sm:mb-16">Hoe ziet de opleiding er uit?</h2>
-                
-                    <div className="grid gap-8 sm:gap-12 sm:grid-cols-2">
+
+                    <div className="flex flex-col gap-12 sm:grid sm:gap-12 sm:grid-cols-2">
                         {programmaLines.map((line, index) => (
-                            <div key={line.id} className={index === programmaLines.length - 1 ? (index + 1)/2 !== 0 ? 'col-span-2' : '' : ''}>
+                            <div key={line.id} className={index === programmaLines.length - 1 ? (index + 1) / 2 !== 0 ? 'col-span-2' : '' : ''}>
                                 <h3 className="font-manuka text-4xl sm:text-6xl uppercase mb-2 sm:mb-4">{line.title}</h3>
                                 <p>{line.description}</p>
                             </div>
@@ -48,7 +50,21 @@ export default function Opleiding() {
                 </div>
             }
 
+            {programmaLines && image &&
+                <div className='flex my-20 justify-between'>
+                    <img src={deco3} alt="decoration" className="h-20" />
+                    <img src={deco3} alt="decoration" className="h-20 hidden sm:block" />
+                </div>
+            }
 
+            {image &&
+
+                <div className='p-6'>
+                    <TitleSection title={image.title} TitleTag='h2' description={image.description} />
+
+                    <img src={image.url} alt={image.title} className="w-full" />
+                </div>
+            }
 
 
 

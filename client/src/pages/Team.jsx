5bee138fe1588e8d11a261e7a5225d515e85ed94
meesakveld <meesakveld@gmail.com>
2024-05-28 12:32:26 +0200

    Updated Team page with Apollo client and ListLayout component

--- client/src/pages/Team.jsx
+++ client/src/pages/Team.jsx
@@ -1,15 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+
 import TopSection from '../components/TopSection'
+
+import { useQuery } from '@apollo/client'
+import { GET_ALL_EMPLOYEES } from '../graphql/team-queries'
+
+import { loadErrorMessages, loadDevMessages } from "@apollo/client/dev";
+import ListLayout from '../components/ListLayout'
 
 export default function Team() {
     
-    const description = "Ontdek het studentenwerk van het graduaat Programmeren! Benieuwd wat onze programmeurs uitspoken in de cloud? Of wat ze kunnen maken met HTML, CSS, JavaScript Node.js, PHP en veel meer? Je vindt het allemaal op dit portfolio platform."
+    loadDevMessages(); loadErrorMessages();
+
+    const [employees, setEmployees] = useState([])
+    const [page, setPage] = useState({})
+
+    const { loading, error, data } = useQuery(GET_ALL_EMPLOYEES)
+
+    useEffect(() => {
+        if (!data) return
+        const formattedEmployees = data.employees.map(employee => {
+            return {
+                ...employee,
+                functions: employee.functions.join(', ')
+            }
+        })
+        setEmployees(formattedEmployees)
+        setPage(data.pages[0])
+    }, [data])
 
     return (
 
         <main className="max-w-200 mx-auto">
 
-            <TopSection title="Team" description={description} />
+            <TopSection title={page.title} description={page.description} />
+
+            {loading && <p className='p-6'>Loading...</p>}
+            {error && <p className='p-6'>Error: {error.message}</p>}
+
+            <ListLayout data={employees} left='name' right='functions' isLink={false} />
 
         </main>
 

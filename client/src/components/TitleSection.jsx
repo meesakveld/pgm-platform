407a669c3b0993bb50396511672e451ab3224724
meesakveld <meesakveld@gmail.com>
2024-05-29 13:58:04 +0200

    Updated TitleSection component to use default props for TitleTag

--- client/src/components/TitleSection.jsx
+++ client/src/components/TitleSection.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import CLink from './CLink'
 
-export default function TitleSection({ title, TitleTag, description, url, urlText }) {
+export default function TitleSection({ title, TitleTag = "h2", description, url, urlText }) {
     return (
         <div className='sm:flex gap-16 items-end mt-6 mb-20'>
             <TitleTag className="font-manuka text-6xl sm:text-8xl uppercase">{title}</TitleTag>

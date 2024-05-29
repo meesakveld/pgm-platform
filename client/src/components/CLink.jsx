5a370037189e1c36fe57154e1a52c8ce1d84c2c7
meesakveld <meesakveld@gmail.com>
2024-05-29 13:57:45 +0200

    Added className prop to CLink component

--- client/src/components/CLink.jsx
+++ client/src/components/CLink.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import arrow from '../assets/icons/arrow.svg'
 
-export default function CLink({ url, text, arrowDeg }) {
+export default function CLink({ url, text, arrowDeg, className }) {
 
 
     return (
-        <Link to={url} className='gap-4 items-center flex w-fit'>
+        <Link to={url} className={`gap-4 items-center flex w-fit ${className}`}>
             <span className='block bg-black' style={{transform: `rotate(${arrowDeg}deg)`, height: 31, width: 31, maskImage: `url("${arrow}")`, maskRepeat: 'no-repeat', maskPosition: 'center'}}></span>
             <p className="inline-block font-shareTechMono text-xl">{text}</p>
         </Link>
